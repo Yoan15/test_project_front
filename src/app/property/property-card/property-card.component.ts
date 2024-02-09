1a8ec7ce1aeeba49da28e3352fbb711e833456ea
Yoan15 <yoann.deco@gmail.com>
2024-02-09 22:20:23 +0100

    style card + dummy data pour style card

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -6,6 +6,12 @@ import { Component } from "@angular/core";
     templateUrl: 'property-card.component.html',
     styleUrl: 'property-card.component.scss'
 })
+
 export class PropertyCardComponent {
-    
+    Property: any = {
+        Id: 1,
+        Name: 'Red House',
+        Type: 'House',
+        Price: 100000
+    }
 }
