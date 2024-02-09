e689e3900469b267878f109a11dab192fd4d0e62
Yoan15 <yoann.deco@gmail.com>
2024-02-09 20:25:43 +0100

    importation et affichage component property card sur page d'accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    PropertyCardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
