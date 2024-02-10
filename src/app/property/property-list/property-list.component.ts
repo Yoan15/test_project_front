5b56c51b5869bb34285dd202dd71b07f1b7d3ae2
Yoan15 <yoann.deco@gmail.com>
2024-02-10 21:29:05 +0100

    creation json pour dummy data et recuperation des data

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,54 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { PropertyCardComponent } from '../property-card/property-card.component';
 import { NgFor } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-property-list',
   standalone: true,
   imports: [
     PropertyCardComponent,
-    NgFor
+    NgFor,
+    HttpClientModule
   ],
   templateUrl: './property-list.component.html',
   styleUrl: './property-list.component.scss'
 })
-export class PropertyListComponent {
-  properties: Array<any> = [
-    {
-      Id: 1,
-      Name: 'Red House',
-      Type: 'House',
-      Price: 100000
-    },
-    {
-      Id: 2,
-      Name: 'Blue House',
-      Type: 'House',
-      Price: 120500
-    },
-    {
-      Id: 3,
-      Name: 'Violet Villa',
-      Type: 'Villa',
-      Price: 190405
-    },
-    {
-      Id: 4,
-      Name: 'Pearl White House',
-      Type: 'House',
-      Price: 150700
-    },
-    {
-      Id: 5,
-      Name: 'Micro House',
-      Type: 'House',
-      Price: 60800
-    },
-    {
-      Id: 6,
-      Name: 'Azur Villa',
-      Type: 'Villa',
-      Price: 180980
-    }
-  ]
+export class PropertyListComponent implements OnInit {
+  properties: any;
+
+  constructor(private http: HttpClient) { };
+
+  ngOnInit(): void {
+    this.http.get('data/properties.json').subscribe(
+      data => {
+        this.properties = data;
+        console.log(data)
+      });
+  }
+
 }
