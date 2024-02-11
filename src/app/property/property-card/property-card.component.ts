6c9f6fd9bd41fb7e1a652f58fcbd43bf3b799097
Yoan15 <yoann.deco@gmail.com>
2024-02-11 17:38:36 +0100

    Modification entité + affichage image correspondante

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from "@angular/core";
 import { IProperty } from "../IProperty.interface";
 import { RouterLink } from "@angular/router";
+import { NgIf } from "@angular/common";
 
 @Component({
     selector: 'app-property-card',
     standalone: true,
     templateUrl: 'property-card.component.html',
     styleUrl: 'property-card.component.scss',
-    imports: [RouterLink]
+    imports: [
+        RouterLink,
+        NgIf
+    ]
 })
 
 export class PropertyCardComponent {
