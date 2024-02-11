87307ca144d08c39fb9bd9e1bca583aab0e4d03a
Yoan15 <yoann.deco@gmail.com>
2024-02-11 17:37:52 +0100

    test routing

--- src/app/property/property-detail/property-detail.component.ts
+++ src/app/property/property-detail/property-detail.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-property-detail',
   standalone: true,
   templateUrl: './property-detail.component.html',
-  styleUrls: ['./property-detail.component.css']
+  styleUrls: ['./property-detail.component.css'],
+  imports: [
+    RouterLink
+  ]
 })
 export class PropertyDetailComponent implements OnInit {
   public propertyId: number = 0;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.propertyId = this.route.snapshot.params['id'];
+    this.propertyId = Number(this.route.snapshot.params['id']);
+
+    this.route.params.subscribe((params) => {
+      this.propertyId = Number(params['id']);
+    })
+  }
+
+  onSelectNext() {
+    this.propertyId += 1;
+    this.router.navigate(['property-detail', this.propertyId]);
   }
 
 }
