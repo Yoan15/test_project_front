b7413421f3ec8c38b4280e8483a90daecc13a885
Yoan15 <yoann.deco@gmail.com>
2024-02-11 16:31:55 +0100

    ajout property-detail component + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
 export const routes: Routes = [
     {path: '', component: PropertyListComponent},
     {path: 'rent-property', component: AddPropertyComponent},
     {path: 'add-property', component: AddPropertyComponent},
+    {path: 'property-detail/:id', component: PropertyDetailComponent},
 ];
--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from "@angular/core";
 import { IProperty } from "../IProperty.interface";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-property-card',
     standalone: true,
     templateUrl: 'property-card.component.html',
-    styleUrl: 'property-card.component.scss'
+    styleUrl: 'property-card.component.scss',
+    imports: [RouterLink]
 })
 
 export class PropertyCardComponent {
